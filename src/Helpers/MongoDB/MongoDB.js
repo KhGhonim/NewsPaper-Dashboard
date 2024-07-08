e114bdf594241631b3703be31ebb13ff1bb965da
Khaled <111690888+LoDaBeY@users.noreply.github.com>
2024-07-08 12:56:37 +0300

    Update MongoDB.js

--- src/Helpers/MongoDB/MongoDB.js
+++ src/Helpers/MongoDB/MongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(${process.env.NEXT_PUBLIC_API_MONGODB});
+    await mongoose.connect(`mongodb+srv://khaledonem89:tF9o4zcbk5F1yJzY@newspaper.3cepbsr.mongodb.net/Data?retryWrites=true&w=majority&appName=NewsPaper`);
     console.log("connected to MongoDB");
   } catch (error) {
     console.log("ERROR WITH CONNECTING MongoDB", error.message);

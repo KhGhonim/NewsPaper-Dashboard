30981369ea8312e48c350c40ce0594f1bd10f263
Khaled <khaledonem89@gmail.com>
2024-07-18 00:50:21 +0300

    test

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -41,7 +41,7 @@ export default function Comments() {
     };
 
     GetComments();
-  }, []);
+  }, [CommentHandler]);
   return (
     <div className="max-w-2xl mx-auto p-4  rounded-lg shadow-md">
       <h2 className="text-2xl font-semibold mb-4">Comments</h2>

fed01c4edbca83d52dbbb94b956fb004005692c8
Khaled <khaledonem89@gmail.com>
2024-08-20 16:54:29 +0300

    MongoDB Storage

--- src/Helpers/MongoDB/UserMongoDB/UserMongoDB.js
+++ src/Helpers/MongoDB/UserMongoDB/UserMongoDB.js
@@ -4,6 +4,7 @@ export const MongoDB = async () => {
   try {
     await mongoose.connect(process.env.NEXT_PUBLIC_API_MONGODB);
     console.log("connected to MongoDB");
+    
   } catch (error) {
     console.log("ERROR WITH CONNECTING MongoDB", error.message);
   }
